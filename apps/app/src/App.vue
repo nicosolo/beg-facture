eacf13b6410b19d0676adcbe48db59ab1865e4de
Nico <nico@solioz.me>
2026-01-24 18:20:37 +0100

    fix: hide sidebar in print view by adding print:hidden class

--- apps/app/src/App.vue
+++ apps/app/src/App.vue
@@ -320,7 +320,7 @@ html {
             <!-- Overlay sidebar for all viewports -->
             <aside
                 v-if="!isLoginPage"
-                class="fixed top-0 right-0 h-screen flex flex-col w-64 transition-all duration-300 ease-in-out z-30"
+                class="fixed top-0 right-0 h-screen flex flex-col w-64 transition-all duration-300 ease-in-out z-30 print:hidden"
                 :class="[isSidebarOpen ? 'translate-x-0' : 'translate-x-full']"
             >
                 <!-- Sidebar component -->
